71f7a69e6a63df9c045cc9a03f21dda6d3857a88
athabeti <abhiselec@gmail.com>
2020-07-14 10:02:08 -0700

    Create RemoteDirectorySimulatorApplicationTests.java

---
+++ src/test/java/com/vad/RemoteDirectorySimulator/RemoteDirectorySimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vad.RemoteDirectorySimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteDirectorySimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
