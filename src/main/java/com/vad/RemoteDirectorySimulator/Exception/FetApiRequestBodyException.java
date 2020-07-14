b8566052db367def65a78e2a5b6876d2807f19bc
athabeti <abhiselec@gmail.com>
2020-07-14 09:19:08 -0700

    Create FetApiRequestBodyException.java

---
+++ src/main/java/com/vad/RemoteDirectorySimulator/Exception/FetApiRequestBodyException.java
@@ -0,0 +1,14 @@
+package com.vad.RemoteDirectorySimulator.Exception;
+
+public class FetApiRequestBodyException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6528970680687963694L;
+
+    public FetApiRequestBodyException(String msg) {
+        super(msg);
+    }
+
+}
