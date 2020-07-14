4e6747131721c8ec05cc3eaafb8d00bcfde93828
athabeti <abhiselec@gmail.com>
2020-07-14 09:21:19 -0700

    Create FetchAliasResponseException.java

---
+++ src/main/java/com/vad/RemoteDirectorySimulator/Exception/FetchAliasResponseException.java
@@ -0,0 +1,13 @@
+package com.vad.RemoteDirectorySimulator.Exception;
+
+public class FetchAiasResponseException extends RuntimeException{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6131825720874042718L;
+
+    public FetchAiasResponseException(String msg) {
+        super(msg);
+    }
+}
