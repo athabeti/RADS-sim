e8b9bf39d19861ad2db3770494f91817757dfaa6
athabeti <abhiselec@gmail.com>
2020-07-14 09:22:43 -0700

    Create FetchAliasNoDataException.java

---
+++ src/main/java/com/vad/RemoteDirectorySimulator/Exception/FetchAliasNoDataException.java
@@ -0,0 +1,13 @@
+package com.vad.RemoteDirectorySimulator.Exception;
+
+public class FetchAliasNoDataException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1301617856725784526L;
+
+    public FetchAliasNoDataException(String msg) {
+        super(msg);
+    }
+}
